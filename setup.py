e3639a4bde59671e3e0cc1d2cad82c0ff8ef2465
Jenkins Continuous Integration Server <jenkins@i-0d044548a5ba54905.instanceid.glomex.cloud>
2017-04-28 09:10:20 +0000

    Bump version: 0.0.12 → 0.0.13

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ dependency_links = [x.strip().replace('git+', '') for x in all_reqs if 'git+' no
 
 setup(
     name='gcdt-say-hello',
-    version='0.0.12',
+    version='0.0.13',
     description='Plugin (gcdt-say-hello) for gcdt',
     long_description=long_description,
     license='MIT',
